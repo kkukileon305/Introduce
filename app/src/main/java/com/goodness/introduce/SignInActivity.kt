06496759a7a7e77ca9ca417aa2481304260f649f
kkukileon305 <kkukileon305@gmail.com>
2023-12-15 16:29:33 +0900

    fix null error (get email into onClick Listener), add Toast message when user input empty value

--- app/src/main/java/com/goodness/introduce/SignInActivity.kt
+++ app/src/main/java/com/goodness/introduce/SignInActivity.kt
@@ -6,24 +6,32 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class SignInActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_signin)
 
-		val email = findViewById<EditText>(R.id.et_email).text
-		val password = findViewById<EditText>(R.id.et_password).text
 
 		findViewById<Button>(R.id.btn_signin).setOnClickListener {
+			val email = findViewById<EditText>(R.id.et_email).text.toString()
+			val password = findViewById<EditText>(R.id.et_password).text.toString()
+
 			if (email.isNotEmpty() && password.isNotEmpty()) {
 				val intent = Intent(this, HomeActivity::class.java)
 
 				intent.putExtra("email", email)
-				intent.putExtra("password", password)
-
 				startActivity(intent)
+			} else {
+				Toast.makeText(this, "아이디/비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
 			}
+		}
+
+		findViewById<Button>(R.id.btn_signup).setOnClickListener {
+			val intent = Intent(this, SignUpActivity::class.java)
+
+			startActivity(intent)
 		}
 	}
 }
