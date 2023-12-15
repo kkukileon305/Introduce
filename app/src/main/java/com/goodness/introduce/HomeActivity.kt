7c4e2e6bd2abd5e5db14bcc5d03de97716c7f433
kkukileon305 <kkukileon305@gmail.com>
2023-12-15 16:27:56 +0900

    create home activity

--- app/src/main/java/com/goodness/introduce/HomeActivity.kt
+++ app/src/main/java/com/goodness/introduce/HomeActivity.kt
@@ -2,10 +2,22 @@ package com.goodness.introduce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_home)
+
+		val email = intent.getStringExtra("email")
+		val emailTextView = findViewById<TextView>(R.id.txt_email)
+
+		email?.let { emailTextView.text = it }
+
+		findViewById<Button>(R.id.btn_finish).setOnClickListener {
+			finish()
+		}
 	}
 }
