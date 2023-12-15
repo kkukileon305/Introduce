e3366fcbea449484fc2834a7cbd0d0f388b76a19
kkukileon305 <kkukileon305@gmail.com>
2023-12-15 16:29:44 +0900

    create signup activity

--- app/src/main/java/com/goodness/introduce/SignUpActivity.kt
+++ app/src/main/java/com/goodness/introduce/SignUpActivity.kt
@@ -1,11 +1,28 @@
 package com.goodness.introduce
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignUpActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_signup)
+
+		val name = findViewById<EditText>(R.id.et_name).text
+		val email = findViewById<EditText>(R.id.et_email).text
+		val password = findViewById<EditText>(R.id.et_password).text
+
+		findViewById<Button>(R.id.btn_signup).setOnClickListener {
+			if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+				finish()
+			} else {
+				Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
+			}
+		}
+
 	}
 }
